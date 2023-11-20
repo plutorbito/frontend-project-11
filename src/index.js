df7415296bc183f3adeb2e2f15c2893c3f73436f
plutorbito <plutorbito@gmail.com>
2023-11-20 20:03:57 +0100

    validation refactoring

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
 import './styles.scss';
 import 'bootstrap';
 import * as yup from 'yup';
-import _ from 'lodash';
 import onChange from 'on-change';
 
 const formEl = document.querySelector('.rss-form');
 const inputEl = document.querySelector('#url-input');
 
-const schema = yup.string().url().required();
-
-const validate = (fields) => {
-  try {
-    schema.validateSync(fields, { abortEarly: false });
-    return {};
-  } catch (e) {
-    return { error: e.inner[0].message };
-  }
-};
-
 const state = {
   status: '',
   errors: '',
   urls: [],
+};
+
+const validate = (fields, array) => {
+  const schema = yup.string().url().required().notOneOf(array);
+  try {
+    schema.validateSync(fields, { abortEarly: false });
+    return '';
+  } catch (e) {
+    return e.message;
+  }
 };
 
 const render = () => (path, value) => {
@@ -31,7 +29,7 @@ const render = () => (path, value) => {
     } else if (value === 'valid') {
       inputEl.classList.remove('is-invalid');
       inputEl.focus();
-      formEl.reset();
+      // formEl.reset();
     }
   }
 };
@@ -42,12 +40,10 @@ formEl.addEventListener('submit', (e) => {
   e.preventDefault();
   const formData = new FormData(e.target);
   const url = formData.get('url');
-  const validation = validate(url);
-  if (!_.isEmpty(validation.error)) {
-    watchedState.errors = validation.error;
-    watchedState.status = 'invalid';
-  } else if (state.urls.includes(url)) {
-    watchedState.errors = 'url already exists';
+  const validation = validate(url, state.urls);
+  console.log(validation);
+  if (validation) {
+    watchedState.errors = validation;
     watchedState.status = 'invalid';
   } else {
     watchedState.errors = '';
