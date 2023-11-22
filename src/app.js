0561398c8054847b4f388408915c2c929d3ff021
plutorbito <plutorbito@gmail.com>
2023-11-22 14:30:00 +0100

    custom error messages added

--- src/app.js
+++ src/app.js
@@ -3,11 +3,14 @@ import 'bootstrap';
 import * as yup from 'yup';
 import onChange from 'on-change';
 import render from './view.js';
+import i18next from 'i18next';
+import resources from './locales/ru.js';
 
 export default () => {
   const elements = {
     formEl: document.querySelector('.rss-form'),
     inputEl: document.querySelector('#url-input'),
+    feedbackEl: document.querySelector('.feedback'),
   };
 
   const state = {
@@ -21,22 +24,43 @@ export default () => {
     return schema.validate(field);
   };
 
-  const watchedState = onChange(state, render(elements));
+  const defaultLanguage = 'ru';
+  const i18nextInstance = i18next.createInstance();
+  i18nextInstance
+    .init({
+      lng: defaultLanguage,
+      debug: false,
+      resources,
+    })
+    .then(() => {
+      yup.setLocale({
+        mixed: {
+          default: i18nextInstance.t('validation.default'),
+          notOneOf: i18nextInstance.t('validation.notOneOf'),
+        },
+        string: {
+          required: i18nextInstance.t('validation.required'),
+          url: i18nextInstance.t('validation.url'),
+        },
+      });
 
-  elements.formEl.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const url = formData.get('url');
-    validate(url, state.urls)
-      .then(() => {
-        watchedState.errors = '';
-        watchedState.status = 'valid';
-        watchedState.urls.push(url);
-      })
-      .catch((error) => {
-        watchedState.errors = error.message;
-        watchedState.status = 'invalid';
-      })
-      .then(() => console.log(state));
-  });
+      const watchedState = onChange(state, render(elements));
+
+      elements.formEl.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const url = formData.get('url');
+        validate(url, state.urls)
+          .then(() => {
+            watchedState.errors = '';
+            watchedState.status = 'valid';
+            watchedState.urls.push(url);
+          })
+          .catch((error) => {
+            watchedState.errors = error.message;
+            watchedState.status = 'invalid';
+          })
+          .then(() => console.log(state));
+      });
+    });
 };
---
+++ src/locales/ru.js
@@ -0,0 +1,12 @@
+export default {
+  ru: {
+    translation: {
+      validation: {
+        default: 'Поле заполнено неверно',
+        required: 'Поле обязательно для заполнения',
+        notOneOf: 'Значение должно быть уникальным',
+        url: 'Некорректный URL',
+      },
+    },
+  },
+};
