a6c2ed9266fe066cb3a1fc382064209ecb488b18
plutorbito <plutorbito@gmail.com>
2023-11-24 11:23:11 +0100

    parseDataFromUrl module added

---
+++ src/parseDataFromUrl.js
@@ -0,0 +1,22 @@
+export default (data, url) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(data, 'text/html');
+  const isRssChanel = doc.querySelector('channel');
+  if (!isRssChanel) {
+    throw new Error('validation.invalidRss');
+  }
+  const feedTitle = doc.querySelector('title').textContent;
+
+  const feedDescription = doc.querySelector('description').textContent;
+  const feedId = _.uniqueId();
+  const items = doc.querySelectorAll('item');
+  const posts = Array.from(items).map((item) => {
+    const postLink = item
+      .querySelector('link')
+      .nextSibling.textContent.split('\\n')[0];
+    const postTitle = item.querySelector('title').textContent;
+    const postId = _.uniqueId();
+    return { postTitle, postLink, feedId: feedId, id: postId };
+  });
+  return { feeds: { feedTitle, feedDescription, url, feedId }, posts };
+};
