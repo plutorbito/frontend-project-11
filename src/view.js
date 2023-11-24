91965996670687e0eb69f244319ec780335be33f
plutorbito <plutorbito@gmail.com>
2023-11-24 11:26:10 +0100

    view updated

--- src/view.js
+++ src/view.js
@@ -1,11 +1,100 @@
-export default (elements) => (path, value) => {
-  if (path === 'errors' && value) {
-    elements.inputEl.classList.add('is-invalid');
-    elements.feedbackEl.textContent = value;
-  } else if (path === 'status' && value === 'valid') {
-    elements.inputEl.classList.remove('is-invalid');
-    elements.inputEl.focus();
-    elements.formEl.reset();
-    elements.feedbackEl.textContent = '';
+const createCardElements = (container, i18nextInstance, title) => {
+  container.innerHTML = '';
+
+  const cardEl = document.createElement('div');
+  cardEl.classList.add('card', 'border-0');
+  container.append(cardEl);
+
+  const cardBodyEl = document.createElement('div');
+  cardBodyEl.classList.add('card-body');
+  cardEl.append(cardBodyEl);
+
+  const cardTitle = document.createElement('h2');
+  cardTitle.classList.add('card-title', 'h4');
+  cardTitle.textContent = i18nextInstance.t(title);
+  cardBodyEl.append(cardTitle);
+
+  const ulEl = document.createElement('ul');
+  cardEl.append(ulEl);
+  return ulEl;
+};
+
+export default (elements, i18nextInstance) => (path, value) => {
+  const cardTitle = `rss.${path}`;
+  switch (path) {
+    case 'status':
+      if (value === 'valid') {
+        elements.inputEl.classList.remove('is-invalid');
+        elements.feedbackEl.classList.remove('text-danger');
+        elements.feedbackEl.classList.add('text-success');
+        elements.formEl.reset();
+        elements.inputEl.focus();
+      } else if (value === 'invalid') {
+        elements.inputEl.classList.add('is-invalid');
+        elements.feedbackEl.classList.remove('text-success');
+        elements.feedbackEl.classList.add('text-danger');
+      }
+      break;
+
+    case 'feedback':
+      elements.feedbackEl.textContent = i18nextInstance.t(value);
+      break;
+
+    case 'feeds':
+      const feedUlEl = createCardElements(
+        elements.feedsEl,
+        i18nextInstance,
+        cardTitle
+      );
+
+      value.forEach((el) => {
+        const feedLiEl = document.createElement('li');
+        feedLiEl.classList.add('list-group-item', 'border-0', 'border-end-0');
+
+        const feedTitleEl = document.createElement('h3');
+        feedTitleEl.classList.add('h6', 'm-0');
+        feedTitleEl.textContent = el.feedTitle;
+        feedLiEl.append(feedTitleEl);
+
+        const feedDescriptionEl = document.createElement('p');
+        feedDescriptionEl.classList.add('m-0', 'small', 'text-black-50');
+        feedDescriptionEl.textContent = el.feedDescription;
+        feedLiEl.append(feedDescriptionEl);
+        feedUlEl.append(feedLiEl);
+      });
+      break;
+
+    case 'posts':
+      const postUlEl = createCardElements(
+        elements.postsEl,
+        i18nextInstance,
+        cardTitle
+      );
+
+      value.forEach((el) => {
+        const postLiEl = document.createElement('li');
+        postLiEl.classList.add(
+          'list-group-item',
+          'd-flex',
+          'justify-content-between',
+          'align-items-start',
+          'border-0',
+          'border-end-0'
+        );
+
+        const postAEl = document.createElement('a');
+        postAEl.href = el.postLink;
+        postAEl.textContent = el.postTitle;
+        postAEl.classList.add('fw-bold');
+        postAEl.dataset.id = el.id;
+        postAEl.target = '_blank';
+        postAEl.rel = 'noopener noreferrer';
+        postLiEl.append(postAEl);
+        postUlEl.append(postLiEl);
+      });
+      break;
+
+    default:
+      throw new Error('Unknown path!');
   }
 };
