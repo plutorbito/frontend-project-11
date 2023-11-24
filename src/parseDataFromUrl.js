bb2ca5a56d2f7ede5fa7877ec40a9344c111505f
plutorbito <plutorbito@gmail.com>
2023-11-24 17:17:13 +0100

    refactoring

--- src/parseDataFromUrl.js
+++ src/parseDataFromUrl.js
@@ -1,5 +1,3 @@
-import _ from 'lodash';
-
 export default (data, url) => {
   const parser = new DOMParser();
   const doc = parser.parseFromString(data, 'text/html');
@@ -10,15 +8,14 @@ export default (data, url) => {
   const feedTitle = doc.querySelector('title').textContent;
 
   const feedDescription = doc.querySelector('description').textContent;
-  const feedId = _.uniqueId();
   const items = doc.querySelectorAll('item');
   const posts = Array.from(items).map((item) => {
     const postLink = item
       .querySelector('link')
       .nextSibling.textContent.split('\\n')[0];
     const postTitle = item.querySelector('title').textContent;
-    const postId = _.uniqueId();
-    return { postTitle, postLink, feedId: feedId, id: postId };
+    return { postTitle, postLink };
   });
-  return { feeds: { feedTitle, feedDescription, url, feedId }, posts };
+  const feeds = [{ feedTitle, feedDescription, url }];
+  return { feeds, posts };
 };
