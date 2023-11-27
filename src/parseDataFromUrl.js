116a6429b3ce465f7284aeb8397d62f181bbb893
plutorbito <plutorbito@gmail.com>
2023-11-27 13:40:24 +0100

    parseDataFromUrl updated

--- src/parseDataFromUrl.js
+++ src/parseDataFromUrl.js
@@ -1,8 +1,8 @@
 export default (data, url) => {
   const parser = new DOMParser();
-  const doc = parser.parseFromString(data, 'text/html');
-  const isRssChanel = doc.querySelector('channel');
-  if (!isRssChanel) {
+  const doc = parser.parseFromString(data, 'text/xml');
+  const isParseError = doc.querySelector('parsererror');
+  if (isParseError) {
     throw new Error('validation.invalidRss');
   }
   const feedTitle = doc.querySelector('title').textContent;
@@ -10,7 +10,7 @@ export default (data, url) => {
   const feedDescription = doc.querySelector('description').textContent;
   const items = doc.querySelectorAll('item');
   const posts = Array.from(items).map((item) => {
-    const postLink = item.querySelector('link').nextSibling.textContent.trim();
+    const postLink = item.querySelector('link').textContent;
     const postTitle = item.querySelector('title').textContent;
     return { postTitle, postLink };
   });
